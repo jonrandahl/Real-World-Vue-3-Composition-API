de01274596ff37c1a507a9b824c175e47add4c47
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2024-01-04 22:21:53 +0000

    feat: initial registration example

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
 
 const props = defineProps({
   event: {
@@ -6,8 +8,16 @@ const props = defineProps({
     required: true,
   },
 })
+
+const register = () => {
+  // If registration API call is successful
+  // Push back to the event details
+  router.push({
+    name: 'EventDetails'
+  })}
 </script>
 
 <template>
   <p>Register for event here</p>
+  <button @click="register">Register Me!</button>
 </template>
